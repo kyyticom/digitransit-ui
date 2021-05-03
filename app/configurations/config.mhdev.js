3de8f1f9dddd85cb33f8d9fc907c184b1181444a
Victor Pavlushkov <victor@kyyti.com>
2021-05-03 10:01:04 +0300

    chore: add MQTT configs for Riihimaki

--- app/configurations/config.mhdev.js
+++ app/configurations/config.mhdev.js
@@ -64,7 +64,7 @@ export default configMerger(walttiConfig, {
   logo: 'mhdev/mh-logo.png',
   favicon: './app/configurations/images/mhdev/mh-favicon.png',
 
-  feedIds: ['Matkahuolto'],
+  feedIds: ['Matkahuolto', 'Riihimäki'],
 
   searchParams: {
     'boundary.rect.min_lat': minLat,
@@ -223,4 +223,7 @@ export default configMerger(walttiConfig, {
       },
     ],
   },
+
+  showAllBusses: true,
+  showVehiclesOnStopPage: true,
 });
